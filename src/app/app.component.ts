f91f648b501f773a02708b612ce01b1c4a9280a9
Genchi Silvio <S.Genchi@almaviva.it>
2023-03-31 13:27:10 +0200

    nuovi file

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,14 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
   title = 'RecipesApp';
+  clickedButton = false;
+
+  receiveBoolean(isRecipesClicked: boolean) {
+    this.clickedButton = isRecipesClicked;
+    console.log(this.clickedButton);
+  }
 }
--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,10 +1,15 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Output } from '@angular/core';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
-  styleUrls: ['./header.component.css']
+  styleUrls: ['./header.component.css'],
 })
 export class HeaderComponent {
+  @Output() recipeShow = new EventEmitter<boolean>();
 
+  recipesClick(e: { preventDefault: () => void }) {
+    e.preventDefault();
+    this.recipeShow.emit(true);
+  }
 }
